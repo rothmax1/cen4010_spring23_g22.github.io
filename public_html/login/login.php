b56d69ee000d1969b3d3eab58270f0e2c4a15d5f
rothmax1 <124603987+rothmax1@users.noreply.github.com>
2023-04-11 15:09:14 -0400

    Update login.php

--- public_html/login/login.php
+++ public_html/login/login.php
@@ -24,8 +24,14 @@ if (empty($_POST["username"])) {
 if (empty($_POST["password"])){
 	die("Password is required.");
 }
-echo "still good";
-$sql = "SELECT * FROM user_login WHERE name="'$name' AND password='$userpassword'"; 
+
+$sql = "SELECT * FROM user_login WHERE name='$name' AND password='$userpassword'"; 
 $result = mysqli_query($mysqli, $sql);
-echo "still good";
+
+if ($result->num_rows>0){
+	$_SESSION['loggedin']=true;
+	header("Location: http://lamp.cse.fau.edu/~cen4010-sp23-g22/");;
+} else{
+	echo "Username or Password incorrect.";
+}
 ?>
