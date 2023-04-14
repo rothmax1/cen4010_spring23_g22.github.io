9902e4a26d41994eccfcd6f4bbde8e7d319f28be
rothmax1 <124603987+rothmax1@users.noreply.github.com>
2023-04-14 15:39:01 -0400

    Update signup.php

--- public_html/signup/signup.php
+++ public_html/signup/signup.php
@@ -4,7 +4,6 @@ include 'database.php';
 
 $name = $_POST["username"];
 $email = $_POST["email"];
-$user_password = $_POST["password"];
 
 if (empty($_POST["username"])) {
 	die("Name is required.");
@@ -14,14 +13,18 @@ if (empty($_POST["password"])){
 	die("Password is required.");
 }
 
+if (! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+	die("Valid email is required.");	
+}
 
 if ($_POST["password"] !== $_POST["password_confirmation"]){
 	die("The passwords entered do not match.");
 }
 
+$password_hash=password_hash($_POST["password"], PASSWORD_BCRYPT);
 
 $sql = "INSERT INTO user_login (name, email, password)
-	VALUES('$name', '$email', '$user_password')";
+	VALUES('$name', '$email', '$password_hash')";
 
 if ($mysqli->query($sql) === TRUE) {
   header("Location: http://lamp.cse.fau.edu/~cen4010-sp23-g22/login/");
