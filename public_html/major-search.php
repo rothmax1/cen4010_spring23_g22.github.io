689d0bb61437a44963f9cefbb3d1a8bc1c8b9c15
rothmax1 <124603987+rothmax1@users.noreply.github.com>
2023-04-14 11:49:28 -0400

    Create major-search.php

---
+++ public_html/major-search.php
@@ -0,0 +1,11 @@
+<?php
+include 'database.php';
+$search_term = $_POST["search"];
+$sql = "SELECT * FROM majors WHERE name LIKE '%{$search_term}%'";
+$result = mysqli_query($mysqli, $sql);
+
+while ($row = mysqli_fetch_assoc($result)){
+	$name = $row['name'];
+	echo $name . "<br>";
+}
+?>
