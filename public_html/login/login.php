b25437d690c597b3c562f410944d3e6908846b13
rothmax1 <124603987+rothmax1@users.noreply.github.com>
2023-04-14 11:49:57 -0400

    Update login.php

--- public_html/login/login.php
+++ public_html/login/login.php
@@ -2,19 +2,11 @@
 
 session_start();
 
-$host = "localhost";
-$dbname = "cen4010sp23g22";
-$username = "cen4010sp23g22";
-$password = "ASpring#2023";
+include 'database.php';
 
 $name = $_POST["username"];
 $userpassword = $_POST["password"];
 
-$mysqli = new mysqli($host, $username, $password, $dbname);
-
-if ($mysqli->connect_errno){
-	die("Connection error: " . $mysqli->connect_error);
-}
 
 if (empty($_POST["username"])) {
 	die("Name is required.");
