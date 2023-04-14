5f17be51dde5dbdc56c83be4fbe0ecd3d0f44479
rothmax1 <124603987+rothmax1@users.noreply.github.com>
2023-04-14 11:47:57 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,9 +2,23 @@
 session_start();
 echo "hello world!";
 
+
+
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true){
 	header("Location: http://lamp.cse.fau.edu/~cen4010-sp23-g22/login/");
 }
-exit();
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang=eng>
+<head>
+<meta charset="utf-8" />
+</head>
+<body>
+<form method="post" action="major-search.php">
+<input type="text" name="search">
+</form>
+</body>
+</html>
