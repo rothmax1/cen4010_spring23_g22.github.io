ead01742508f5949011bc9d1ea63729d53065957
rothmax1 <124603987+rothmax1@users.noreply.github.com>
2023-04-14 22:31:07 -0400

    Update major-search.php

--- public_html/major-search.php
+++ public_html/major-search.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang=eng>
+<head>
+<meta charset="utf-8" />
+</head>
+<h1>The Integrator</h1>
+<body>
+<form method="post" action="major-search.php">
+<h2>Search Majors</h2>
+<input type="text" name="search">
+</form>
+<form method="post" action="career-path-search.php">
+<h2>Search Career Paths</h2>
+<input type="text" name="search">
+</form>
+</body>
+</html>
+
 <?php
 include 'database.php';
 $search_term = $_POST["search"];
@@ -5,9 +23,10 @@ $sql = "SELECT * FROM majors WHERE name LIKE '%{$search_term}%'";
 $result = mysqli_query($mysqli, $sql);
 
 while ($row = mysqli_fetch_assoc($result)){
+	
 	$name = $row['name'];
 	echo $name . "\t";
 	$link = $row['url'];
-	echo $link . "<br>";
+	echo "<a href='$link'>Link to page</a>" . "<br>";
 }
 ?>
