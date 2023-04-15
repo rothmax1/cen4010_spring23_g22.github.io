8e138483dbcf501275562334d24d669288a285c6
rothmax1 <124603987+rothmax1@users.noreply.github.com>
2023-04-14 22:31:34 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
-echo "hello world!";
+
 
 
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true){
 	header("Location: http://lamp.cse.fau.edu/~cen4010-sp23-g22/login/");
 }
-
 
 ?>
 
@@ -16,11 +15,14 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true){
 <head>
 <meta charset="utf-8" />
 </head>
+<h1>The Integrator</h1>
 <body>
 <form method="post" action="major-search.php">
+<h2>Search Majors</h2>
 <input type="text" name="search">
 </form>
-<form method="post" action="career-paths-search.php">
+<form method="post" action="career-path-search.php">
+<h2>Search Career Paths</h2>
 <input type="text" name="search">
 </form>
 </body>
